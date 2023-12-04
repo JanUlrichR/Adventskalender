bd980cbc9748e9b5a810900124a18241555ebd3d
Jan Ulrich Robens <jan.robens@web.de>
2023-12-05 00:12:22 +0100

    adapt data

--- backend/src/controllers/tuerchenData.ts
+++ backend/src/controllers/tuerchenData.ts
@@ -1,14 +1,14 @@
 import {AdventskalenderConfig, TuerchenConfig} from "./tuerchen";
 
 
-
 const calendarConfig: TuerchenConfig[] = [
+
     {
         "day": 1,
         "md": "Ping",
         "openable": false,
-        "x": 100,
-        "y": 918,
+        "x": 167,
+        "y": 985,
         "width": 100,
         "height": 100
     },
@@ -16,8 +16,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 2,
         "md": "Ping",
         "openable": false,
-        "x": 1315,
-        "y": 657,
+        "x": 1326,
+        "y": 673,
         "width": 100,
         "height": 100
     },
@@ -25,8 +25,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 3,
         "md": "Ping",
         "openable": false,
-        "x": 1726,
-        "y": 1166,
+        "x": 1735,
+        "y": 1165,
         "width": 100,
         "height": 100
     },
@@ -34,8 +34,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 4,
         "md": "Ping",
         "openable": false,
-        "x": 522,
-        "y": 598,
+        "x": 480,
+        "y": 588,
         "width": 100,
         "height": 100
     },
@@ -43,8 +43,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 5,
         "md": "Ping",
         "openable": false,
-        "x": 771,
-        "y": 1004,
+        "x": 796,
+        "y": 1023,
         "width": 100,
         "height": 100
     },
@@ -52,8 +52,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 6,
         "md": "Ping",
         "openable": false,
-        "x": 1982,
-        "y": 897,
+        "x": 1974,
+        "y": 927,
         "width": 100,
         "height": 100
     },
@@ -61,8 +61,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 7,
         "md": "Ping",
         "openable": false,
-        "x": 1551,
-        "y": 618,
+        "x": 1557,
+        "y": 619,
         "width": 100,
         "height": 100
     },
@@ -70,8 +70,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 8,
         "md": "Ping",
         "openable": false,
-        "x": 747,
-        "y": 612,
+        "x": 745,
+        "y": 609,
         "width": 100,
         "height": 100
     },
@@ -79,8 +79,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 9,
         "md": "Ping",
         "openable": false,
-        "x": 1350,
-        "y": 969,
+        "x": 1349,
+        "y": 968,
         "width": 100,
         "height": 100
     },
@@ -88,8 +88,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 10,
         "md": "Ping",
         "openable": false,
-        "x": 2242,
-        "y": 549,
+        "x": 2254,
+        "y": 554,
         "width": 100,
         "height": 100
     },
@@ -97,8 +97,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 11,
         "md": "Ping",
         "openable": false,
-        "x": 181,
-        "y": 590,
+        "x": 198,
+        "y": 613,
         "width": 100,
         "height": 100
     },
@@ -106,8 +106,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 12,
         "md": "Ping",
         "openable": false,
-        "x": 1116,
-        "y": 977,
+        "x": 1120,
+        "y": 984,
         "width": 100,
         "height": 100
     },
@@ -115,7 +115,7 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 13,
         "md": "Ping",
         "openable": false,
-        "x": 617,
+        "x": 622,
         "y": 146,
         "width": 100,
         "height": 100
@@ -124,8 +124,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 14,
         "md": "Ping",
         "openable": false,
-        "x": 1751,
-        "y": 911,
+        "x": 1730,
+        "y": 930,
         "width": 100,
         "height": 100
     },
@@ -133,8 +133,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 15,
         "md": "Ping",
         "openable": false,
-        "x": 409,
-        "y": 939,
+        "x": 429,
+        "y": 952,
         "width": 100,
         "height": 100
     },
@@ -142,8 +142,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 16,
         "md": "Ping",
         "openable": false,
-        "x": 1437,
-        "y": 658,
+        "x": 1443,
+        "y": 656,
         "width": 100,
         "height": 100
     },
@@ -151,8 +151,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 17,
         "md": "Ping",
         "openable": false,
-        "x": 935,
-        "y": 931,
+        "x": 942,
+        "y": 942,
         "width": 100,
         "height": 100
     },
@@ -160,8 +160,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 18,
         "md": "Ping",
         "openable": false,
-        "x": 1005,
-        "y": 672,
+        "x": 1014,
+        "y": 680,
         "width": 100,
         "height": 100
     },
@@ -170,7 +170,7 @@ const calendarConfig: TuerchenConfig[] = [
         "md": "Ping",
         "openable": false,
         "x": 1297,
-        "y": 1116,
+        "y": 1110,
         "width": 100,
         "height": 100
     },
@@ -178,8 +178,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 20,
         "md": "Ping",
         "openable": false,
-        "x": 607,
-        "y": 971,
+        "x": 633,
+        "y": 967,
         "width": 100,
         "height": 100
     },
@@ -187,8 +187,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 21,
         "md": "Ping",
         "openable": false,
-        "x": 1743,
-        "y": 595,
+        "x": 1746,
+        "y": 597,
         "width": 100,
         "height": 100
     },
@@ -196,8 +196,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 22,
         "md": "Ping",
         "openable": false,
-        "x": 300,
-        "y": 441,
+        "x": 295,
+        "y": 428,
         "width": 100,
         "height": 100
     },
@@ -205,8 +205,8 @@ const calendarConfig: TuerchenConfig[] = [
         "day": 23,
         "md": "Ping",
         "openable": false,
-        "x": 1534,
-        "y": 1016,
+        "x": 1536,
+        "y": 1022,
         "width": 100,
         "height": 100
     },
@@ -215,15 +215,16 @@ const calendarConfig: TuerchenConfig[] = [
         "md": "Ping",
         "openable": false,
         "x": 1988,
-        "y": 546,
+        "y": 549,
         "width": 100,
         "height": 100
     }
+
 ]
 
 export const adventskalenderConfig: AdventskalenderConfig = {
     bgUrl: "https://cdn.discordapp.com/attachments/108572403575058432/1180946764728123453/lenbei_snowy_village_at_night_24_houses_advent_calendar_e64baf5c-9c7b-4ffa-8b31-b15428849e44.png?ex=657f4561&is=656cd061&hm=8125c5ad344670e41357884cb46f35f9ced2be05892a9122631e52857e263523&",
-    refHeight: 765,
-    refWidth: 2543,
+    refHeight: 1287,
+    refWidth: 2576,
     tuerchenConfigs: calendarConfig
 }
